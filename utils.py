f81d1239d18ab4aa9b25165cf8bddcfb4950ee3a
Otavio-Parraga <otavio.parraga@acad.pucrs.br>
2021-03-30 18:11:18 -0300

    making code prettier

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import Counter
 
-
+# for read and save files
 def preprocess_input(inpt):
     if ('/' in inpt) or ('\\' in inpt):
         plaintext = read_file(inpt)
@@ -23,25 +23,3 @@ def save_text(text, file_name):
     with open(f'{file_name}.txt', mode='w', encoding='utf-8') as f:
         f.write(text)
     return True
-
-
-def find_index_of_coincidence(text):
-    frequencies = frequency_analysis(text)
-    dividend = np.sum([x * (x - 1) for x in frequencies.values()])
-    divisor = len(text) * (len(text) - 1)
-    return dividend / divisor
-
-
-def frequency_analysis(text):
-    count = Counter(text)
-    return count
-
-
-def check_key_integrity(possible_key):
-    i = (possible_key + possible_key).find(possible_key, 1, -1)
-    return possible_key if i == -1 else possible_key[:i]
-
-
-def get_most_similar_value(list_of_lists, factor=0.0001):
-    list_of_avgs = [np.mean(x) - (factor * i) for i,x in enumerate(list_of_lists) if len(x) != 0]
-    return np.argmax(list_of_avgs) + 1
--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -16,10 +16,6 @@ parser.add_argument('--file_name', default='output',
 parser.add_argument('--key_limit', default=20, type=int,
                     help='max number of key size trial')
 
-letter2idx = {a: i for i, a in enumerate(string.ascii_lowercase)}
-idx2letter = {i: a for a, i in letter2idx.items()}
-ALPHABET_SIZE = len(string.ascii_lowercase)
-
 def find_key_size(full_text):
     values_of_coincidence_idx = [[] for x in range(KEY_LIMIT + 1)]
 
@@ -28,7 +24,8 @@ def find_key_size(full_text):
         for idx, char in enumerate(full_text):
             sub_texts[idx % i].append(char)
         for sub_text in sub_texts:
-            values_of_coincidence_idx[i].append(find_index_of_coincidence(''.join(sub_text)))
+            values_of_coincidence_idx[i].append(
+                find_index_of_coincidence(''.join(sub_text)))
     return get_most_similar_value(values_of_coincidence_idx)
 
 
@@ -49,6 +46,56 @@ def build_chunks_by_order(text_chunks):
     return [''.join(chunk) for chunk in new_text_chunks]
 
 
+def find_index_of_coincidence(text):
+    frequencies = frequency_analysis(text)
+    dividend = np.sum([x * (x - 1) for x in frequencies.values()])
+    divisor = len(text) * (len(text) - 1)
+    return dividend / divisor
+
+
+def frequency_analysis(text):
+    count = Counter(text)
+    return count
+
+
+def get_most_similar_value(list_of_lists, factor=0.0001):
+    list_of_avgs = [np.mean(x) - (factor * i)
+                    for i, x in enumerate(list_of_lists) if len(x) != 0]
+    return np.argmax(list_of_avgs) + 1
+
+
+def decipher_text(ciphertext):
+    """
+    Receives a cipher text as parameter, and run the following steps:
+    find the key size -> split ciphertext into text chunks -> 
+    discover key -> decrypt cipher text
+    """
+    key_size = find_key_size(ciphertext)
+    text_chunks = break_text_into_key_sizes(key_size, ciphertext)
+    text_chunks = build_chunks_by_order(text_chunks)
+
+    key = ''
+
+    for chunk in tqdm(text_chunks, desc='Discovering key'):
+        frequencies = frequency_analysis(chunk)
+        most_common_char = frequencies.most_common(1)[0][0]
+        # since "e" is the most common letter in english
+        key_char_in_position = (
+            letter2idx[most_common_char] + 26 - letter2idx['e']) % ALPHABET_SIZE
+        key += idx2letter[key_char_in_position]
+
+    print(f'Key Size: {len(key)}')
+    print(f'Key: {key}')
+    decrypted_text = []
+
+    for i, char in tqdm(enumerate(ciphertext), desc='Decrypting text'):
+        key_letter = key[i % len(key)]
+        deciphered_char = idx2letter[(
+            letter2idx[char] + 26 - letter2idx[key_letter]) % ALPHABET_SIZE]
+        decrypted_text.append(deciphered_char)
+    return ''.join(decrypted_text)
+
+
 def cipher_text(key, plaintext):
     """
     Receives a key and a plaintext, as string, and 
@@ -58,52 +105,26 @@ def cipher_text(key, plaintext):
     encrypted_text = ''
     for i, letter in enumerate(plaintext):
         key_letter = key[i % len(key)]
-        new_letter = idx2letter[(letter2idx[letter] + letter2idx[key_letter]) % ALPHABET_SIZE]
+        new_letter = idx2letter[(
+            letter2idx[letter] + letter2idx[key_letter]) % ALPHABET_SIZE]
         encrypted_text += new_letter
     return encrypted_text
-
-
-def decipher_text(ciphertext):
-    # finds the possible key size
-    possible_key_size = find_key_size(ciphertext)
-    text_chunks = break_text_into_key_sizes(possible_key_size, ciphertext)
-    text_chunks = build_chunks_by_order(text_chunks)
-
-    key = ''
-
-    for chunk in tqdm(text_chunks, desc='Discovering key'):
-        frequencies = frequency_analysis(chunk)
-        most_common_char = frequencies.most_common(1)[0][0]
-        # since "e" is the most common letter in english
-        key_char_in_position = (letter2idx[most_common_char] + 26 - letter2idx['e']) % ALPHABET_SIZE
-        key += idx2letter[key_char_in_position]
-        # check key integrity, so that some key like "abcabcabc" will become only "abc"
-        key = check_key_integrity(key)
-
-    print(f'Key Size: {len(key)}')
-    print(f'Key: {key}')
-    decrypted_text = []
-
-    for i, char in tqdm(enumerate(ciphertext), desc='Decrypting text'):
-        key_letter = key[i % len(key)]
-        deciphered_char = idx2letter[(letter2idx[char] + 26 - letter2idx[key_letter]) % ALPHABET_SIZE]
-        decrypted_text.append(deciphered_char)
-    return ''.join(decrypted_text)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
+    letter2idx = {a: i for i, a in enumerate(string.ascii_lowercase)}
+    idx2letter = {i: a for a, i in letter2idx.items()}
+    ALPHABET_SIZE = len(string.ascii_lowercase)
     KEY_LIMIT = args.key_limit
 
     if args.key and args.pt:
-        print('Encrypt!')
         key = preprocess_input(args.key)
         plaintext = preprocess_input(args.pt)
         output = cipher_text(key, plaintext)
 
     elif args.ct:
-        print('Decipher!')
         ciphertext = preprocess_input(args.ct)
         output = decipher_text(ciphertext)
     else:
